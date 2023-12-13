8c45b358d61e3c3aea28d22062a40344e0f72e8c
SIMON-KANGI <simonmwangikangi@gmail.com>
2023-12-13 16:11:45 +0300

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const saturdayFun=(activity='roller-skate')=>{
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun('roller-skate')
+
+const mondayWork=(activity='go to the office')=>{
+return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(weight= "*"){
+    return function(params='special'){
+        return `You are ${weight}${params}${weight}!`
+    }
+    
+}
+wrapAdjective("100%")("special")
